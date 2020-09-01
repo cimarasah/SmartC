86a1379e658ae15a1c5123e1d8fadb930d094f77
Cimara Myraelle de Sá Oliveira <cimara.oliveira@amcom.com.br>
2020-08-31 21:30:36 -0300

    criação da estrutura

---
+++ src/SmartC.ApplicationCore/Entities/Paciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartC.ApplicationCore.Entities
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
